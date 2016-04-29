812af40c631b47f3fa38c9b3dc1d72d3715c5348
Francois Perron <francois.perron@ticketmaster.com>
2016-04-29 10:05:09 -0400

    Formatting

--- discovery/src/main/java/com/ticketmaster/api/discovery/operation/SearchEventsOperation.java
+++ discovery/src/main/java/com/ticketmaster/api/discovery/operation/SearchEventsOperation.java
@@ -56,7 +56,7 @@ public class SearchEventsOperation extends PagedOperation<SearchEventsOperation>
 
   public SearchEventsOperation unit(String unit) {
     Preconditions.checkArgument(UNITS.contains(unit),
-        String.format("Invalid unit, supported units: %s", UNITS.toString()));
+            String.format("Invalid unit, supported units: %s", UNITS.toString()));
     return withParam("unit", unit);
   }
 
@@ -76,33 +76,53 @@ public class SearchEventsOperation extends PagedOperation<SearchEventsOperation>
     return withParam("startDateTime", startDateTime);
   }
 
-  public SearchEventsOperation city( String city) {
+  public SearchEventsOperation city(String city) {
     return withParam("city", city);
   }
 
-  public SearchEventsOperation countryCode( String countryCode) { return withParam("countryCode", countryCode); }
+  public SearchEventsOperation countryCode(String countryCode) {
+    return withParam("countryCode", countryCode);
+  }
 
-  public SearchEventsOperation countryCode( String... countryCodes) { return withCommaSeparatedParam("countryCode", countryCodes); }
+  public SearchEventsOperation countryCode(String... countryCodes) {
+    return withCommaSeparatedParam("countryCode", countryCodes);
+  }
 
-  public SearchEventsOperation stateCode( String stateCode) {
+  public SearchEventsOperation stateCode(String stateCode) {
     return withParam("stateCode", stateCode);
   }
 
-  public SearchEventsOperation stateCode( String... stateCodes) { return withCommaSeparatedParam("stateCode", stateCodes); }
+  public SearchEventsOperation stateCode(String... stateCodes) {
+    return withCommaSeparatedParam("stateCode", stateCodes);
+  }
 
-  public SearchEventsOperation dmaId( String dmaId) { return withParam("dmaId", dmaId); }
+  public SearchEventsOperation dmaId(String dmaId) {
+    return withParam("dmaId", dmaId);
+  }
 
-  public SearchEventsOperation dmaId( String... dmaIds) { return withCommaSeparatedParam("dmaIds", dmaIds); }
+  public SearchEventsOperation dmaId(String... dmaIds) {
+    return withCommaSeparatedParam("dmaIds", dmaIds);
+  }
 
-  public SearchEventsOperation includeTBA( String includeTBA) { return withParam("includeTBA", includeTBA); }
+  public SearchEventsOperation includeTBA(String includeTBA) {
+    return withParam("includeTBA", includeTBA);
+  }
 
-  public SearchEventsOperation includeTBD( String includeTBD) { return withParam("includeTBD", includeTBD); }
+  public SearchEventsOperation includeTBD(String includeTBD) {
+    return withParam("includeTBD", includeTBD);
+  }
 
-  public SearchEventsOperation sort( String sort) { return withParam("sort", sort); }
+  public SearchEventsOperation sort(String sort) {
+    return withParam("sort", sort);
+  }
 
-  public SearchEventsOperation onsaleEndDateTime( String onsaleEndDateTime) { return withParam("onsaleEndDateTime", onsaleEndDateTime); }
+  public SearchEventsOperation onsaleEndDateTime(String onsaleEndDateTime) {
+    return withParam("onsaleEndDateTime", onsaleEndDateTime);
+  }
 
-  public SearchEventsOperation onsaleStartDateTime( String onsaleStartDateTime) { return withParam("onsaleStartDateTime", onsaleStartDateTime); }
+  public SearchEventsOperation onsaleStartDateTime(String onsaleStartDateTime) {
+    return withParam("onsaleStartDateTime", onsaleStartDateTime);
+  }
 
   @Override
   protected SearchEventsOperation getThis() {
